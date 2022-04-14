ebbdc9ac421618574065bff686a1b47b64b6f279
Tigran Karakhanyan <lev.98@bk.ru>
2022-04-14 14:05:36 +0400

    Initial Commit

---
+++ SwiftUI_App/SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppApp.swift
+//  SwiftUI_App
+//
+//  Created by Karakhanyan Tigran on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
