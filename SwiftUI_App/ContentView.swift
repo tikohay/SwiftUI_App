42e539948176ed55489e5c0ff6ae967e1a408e43
Tigran Karakhanyan <lev.98@bk.ru>
2022-04-14 14:46:45 +0400

    added alert

--- SwiftUI_App/ContentView.swift
+++ SwiftUI_App/ContentView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isError = false
+    
     var body: some View {
-        Text("Hello, World").padding()
+        showAlert()
+    }
+    
+    fileprivate func showAlert() -> some View {
+        return Button("Button") {
+            self.isError = true
+        }.alert(isPresented: $isError, content: {
+            Alert(title: Text("Test"), message: Text("Hello"), primaryButton: .destructive(Text("hey"), action: {
+                print("test")
+            }), secondaryButton: .cancel())
+        })
     }
 }
 
