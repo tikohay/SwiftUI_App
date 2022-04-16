5c0e836948006e3f82de705737b0e944bf5b1ad7
Tigran Karakhanyan <lev.98@bk.ru>
2022-04-16 18:15:09 +0400

    added form

---
+++ SwiftUI_App/SwiftCourse2/FormView.swift
@@ -0,0 +1,40 @@
+//
+//  FormView.swift
+//  SwiftUI_App
+//
+//  Created by Karakhanyan Tigran on 16.04.2022.
+//
+
+import SwiftUI
+
+struct FormView: View {
+    
+    @State private var colors = ["Red", "Green", "Blue"]
+    @State private var selectedColor = 0
+    @State private var additionalSettings = false
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Colors")) {
+                    Picker("Select color", selection: $selectedColor) {
+                        ForEach.init(0..<colors.count) {
+                            Text(colors[$0])
+                        }
+                    }
+                }
+                Toggle("Additional settings", isOn: $additionalSettings)
+                .navigationBarTitle("Settings")
+                Button("Hello") {
+                    print("hello")
+                }.disabled(additionalSettings)
+            }
+        }
+    }
+}
+
+struct FormView_Previews: PreviewProvider {
+    static var previews: some View {
+        FormView()
+    }
+}
